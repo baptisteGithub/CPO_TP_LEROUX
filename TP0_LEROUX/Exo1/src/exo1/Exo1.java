5722704c0fc4ded7747e6d9d4cbf116cd7813adc
Baptiste Leroux <baptiste.leroux@epfedu.fr>
2023-09-25 15:28:48 +0200

    TP0 Exo1

---
+++ TP0_LEROUX/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+/*Baptiste LEROUX
+TDB
+TP0 partie 1
+25/09/23
+*/
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bapti
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Enchante, j'aimerais connaitre votre prenom !");
+        prenom = sc.nextLine();
+    }
+    
+}

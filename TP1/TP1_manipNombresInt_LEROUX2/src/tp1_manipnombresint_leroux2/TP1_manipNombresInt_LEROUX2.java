c04883f356f5b0a9654a71b687514043eaa7bb44
Baptiste Leroux <baptiste.leroux@epfedu.fr>
2023-09-26 12:36:58 +0200

    TP1 Manip Nombres Int

---
+++ TP1/TP1_manipNombresInt_LEROUX2/src/tp1_manipnombresint_leroux2/TP1_manipNombresInt_LEROUX2.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombresint_leroux2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bapti
+ */
+public class TP1_manipNombresInt_LEROUX2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int entier1;
+        int entier2;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        
+        System.out.println("Saisissez un premier entier");
+        entier1 = sc.nextInt();
+        System.out.println("Saisissez un second entier");
+        entier2 = sc.nextInt();
+        
+        
+        System.out.println(entier1+" et "+entier2);
+        System.out.println("La somme de ces deux nombres est : "+(entier1+entier2));
+        System.out.println("La difference de ces deux nombres est : "+(entier1-entier2));
+        System.out.println("Le produit de ces deux nombres est : "+(entier1*entier2));
+        System.out.println("La division de ces deux nombres est : "+(entier1/entier2)+" et son reste est "+(entier1%entier2));
+        
+        
+        
+        
+                
+    }
+    
+}

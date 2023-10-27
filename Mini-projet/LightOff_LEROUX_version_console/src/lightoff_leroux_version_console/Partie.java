12140e999ee12c628fe1e71d6f734b2a9298828b
Baptiste Leroux <baptiste.leroux@epfedu.fr>
2023-10-27 23:30:59 +0200

    LightOff : la classe GrilleDeCellules a été ajoutée et est  fonctionnelle

---
+++ Mini-projet/LightOff_LEROUX_version_console/src/lightoff_leroux_version_console/Partie.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_leroux_version_console;
+
+/**
+ *
+ * @author bapti
+ */
+public class Partie {
+    
+}
